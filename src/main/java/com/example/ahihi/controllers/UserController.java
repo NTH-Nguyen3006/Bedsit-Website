bec085b1f8209015122a0bc20df99ff8b5c40673
NTH-Nguyen3006 <nthn300607@gmail.com>
2025-07-05 22:48:23 +0700

    5/7/25

--- src/main/java/com/example/ahihi/controllers/UserController.java
+++ src/main/java/com/example/ahihi/controllers/UserController.java
@@ -1,5 +1,27 @@
-package com.example.ahihi.sevices;
+package com.example.ahihi.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ch.qos.logback.core.model.Model;
+
+@Controller
 public class UserController {
 
+    @RequestMapping("/admin")
+    public String adminPage(Model model) {
+        return "admin/index";
+    }
+
+    @RequestMapping("/admin/user/create")
+    public String adminCreateUserPage(Model model) {
+        return "admin/user/create";
+    }
+
+    @RequestMapping("/client")
+    public String clientPage(Model model) {
+        return "hello";
+    }
 }
